aa47fe72eef284c209617f9722defefaeef415c2
polo2013 <paul.zhou@163.com>
2014-03-17 10:31:46 +0800

    初始化

---
+++ SRC/data/conf/db.php
@@ -0,0 +1,11 @@
+<?php
+return array (
+  'DB_TYPE' => 'mysql',
+  'DB_HOST' => 'lzoon.gotoftp4.com',
+  'DB_PORT' => '3306',
+  'DB_NAME' => 'lzoon',
+  'DB_USER' => 'lzoon',
+  'DB_PWD' => 'hhj900712llc',
+  'DB_PREFIX' => 'tp_',
+);
+?>
