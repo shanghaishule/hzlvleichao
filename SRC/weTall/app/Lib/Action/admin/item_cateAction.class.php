8e05789667bbe7b586be1fc54fa9446d1bbbe555
polo2013 <paul.zhou@163.com>
2014-03-20 22:31:44 +0800

    bug

--- SRC/weTall/app/Lib/Action/admin/item_cateAction.class.php
+++ SRC/weTall/app/Lib/Action/admin/item_cateAction.class.php
@@ -29,7 +29,7 @@ class item_cateAction extends backendAction {
                 <td align='center'><input type='checkbox' value='\$id' class='J_checkitem'></td>
                 <td align='center'>\$id</td>
                 <td>\$spacer<span data-tdtype='view' data-field='name' data-id='\$id' class='tdview'  style='color:\$fcolor'>\$name</span></td>
-                <td align='left'>".__PARENTURL__."/weTall/index.php?m=book&a=\$is_allcate</td>
+                <td align='left'>".__PARENTURL__."/weTall/index.php?m=book&a=\$is_allcate&tokenTall=".$this->getTokenTall()."</td>
         		<td align='center'>\$str_img</td>
                 <td align='center'>\$str_type</td>
                 <td align='center'><span data-tdtype='view' data-field='ordid' data-id='\$id' class='tdview'>\$ordid</span></td>
